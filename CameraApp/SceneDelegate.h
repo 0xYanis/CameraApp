0d8503e22b05b2b72c5186e073aa315225836965
Yanis Rybkin <ynchprod@gmail.com>
2024-03-28 01:43:50 +0300

    Initial Commit

---
+++ CameraApp/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  CameraApp
+//
+//  Created by Yanis on 28.03.2024.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
